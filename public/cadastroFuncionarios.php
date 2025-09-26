4a63a349abbb8deaa84078c4161344e4bc33b0be
Caio <caio_ponick@estudante.sesisenai.org.br>
2025-09-26 12:01:31 -0300

    ADD - adicionei o cadastro em php

--- public/cadastroFuncionarios.php
+++ public/cadastroFuncionarios.php
@@ -1,6 +1,41 @@
 <?php
 
+include "db.php";
+
+session_start();
+
+if(empty($_SESSION["user_pk"])){
+    header("Location: index.php");
+    exit;
+};
+
+$register_msg = "";
+if($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['register'])){
+    $new_user = $_POST['nome'] ?? "";
+    $new_func = $_POST['funcao'] ?? "";
+    $new_cpf = $_POST['cpf'] ?? "";
+    $new_ende = $_POST['endereco'] ?? "";
+    $new_cont = $_POST['contato'] ?? "";
+    $new_email = $_POST['email'] ?? "";
+    $new_senha = $_POST['senha'] ?? "";
+    if($new_user && $new_pass){
+        $stmt = $mysqli -> prepare("INSERT INTO usuarios (nome, funcao, cpf, endereco, contato, email, senha) VALUES (?,?,?, ?, ?, ?, ?)");
+        $stmt -> bind_param("sssssss", $new_user, $new_pass,$new_func , $new_cpf, $new_ende, $new_cont, $new_email);
+        
+        if($stmt->execute()) {
+            $register_msg = "Usuário cadastrado com sucesso!";
+        }else{
+            $register_msg = "Erro ao cadastrar novo usuário.";
+        };
+
+        $stmt->close();
+    }else{
+        $register_msg = "Preencha todos os campos.";
+    };
+};
+
 ?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -22,45 +57,43 @@
         <div class="entradas">
             <div class="nomeEntradas">NOME</div>
             <div class="error" id="errorNome"></div>
-            <input class="boxEntradas" id="inputNome"></input>
+            <input class="boxEntradas" id="inputNome" name="nome" type="text"></input>
         </div>
         <div class="entradas">
-            <div class="nomeEntradas">FUNÇÃO</div>
+            <select name="funcao">
+                <option value="administrador">ADM</option>
+                <option value="maquinista" selected>MAQUINISTA</option>
+            </select>
             <div class="error" id="errorFuncao"></div>
             <input class="boxEntradas" id="inputFuncao"></input>
         </div>
         <div class="entradas">
-            <div class="nomeEntradas">Nº REGISTRO</div>
-            <div class="error" id="errorNumero"></div>
-            <input class="boxEntradas" id="inputNumero"></input>
-        </div>
-        <div class="entradas">
             <div class="nomeEntradas">CPF</div>
             <div class="error" id="errorCpf"></div>
-            <input class="boxEntradas" id="inputCpf"></input>
+            <input class="boxEntradas" id="inputCpf" name="cpf"></input>
         </div>
         <div class="entradas">
             <div class="nomeEntradas">ENDEREÇO</div>
             <div class="error" id="errorEndereco"></div>
-            <input class="boxEntradas" id="inputEndereco"></input>
+            <input class="boxEntradas" id="inputEndereco" name="endereco"></input>
         </div>
         <div class="entradas">
             <div class="nomeEntradas">CONTATO</div>
             <div class="error" id="errorCtt"></div>
-            <input class="boxEntradas" id="inputCtt"></input>
+            <input class="boxEntradas" id="inputCtt" name="contato"></input>
         </div>
         <div class="entradas">
             <div class="nomeEntradas">EMAIL</div>
             <div class="error" id="errorEmail"></div>
-            <input class="boxEntradas" id="inputEmail"></input>
+            <input class="boxEntradas" id="inputEmail" name="email"></input>
         </div>
         <div class="entradas">
             <div class="nomeEntradas">SENHA</div>
             <div class="error" id="errorSenha"></div>
-            <input class="boxEntradas" id="inputSenha"></input>
+            <input class="boxEntradas" id="inputSenha" name="senha" type="password"></input>
         </div>
         <div id="buttonCadastro">
-            <input type="button" id="buttonTxt" value="CADASTRAR" onclick="validaCadastro()"></input>
+            <button type="submit" name="register" id="buttonTxt" value="1">CADASTRAR</button>
         </div>
     </main>
 </body>
