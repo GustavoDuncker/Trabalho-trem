19bb552c94732392ccac25c13e70f14fa3f6577c
Caio <caio_ponick@estudante.sesisenai.org.br>
2025-09-26 12:29:55 -0300

    add - incrementei o cadastro

--- public/cadastroFuncionarios.php
+++ public/cadastroFuncionarios.php
@@ -1,6 +1,6 @@
 <?php
 
-include "db.php";
+include "../banco/db.php";
 
 session_start();
 
@@ -16,11 +16,11 @@ if($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['register'])){
     $new_cpf = $_POST['cpf'] ?? "";
     $new_ende = $_POST['endereco'] ?? "";
     $new_cont = $_POST['contato'] ?? "";
-    $new_email = $_POST['email'] ?? "";
-    $new_senha = $_POST['senha'] ?? "";
-    if($new_user && $new_pass){
-        $stmt = $mysqli -> prepare("INSERT INTO usuarios (nome, funcao, cpf, endereco, contato, email, senha) VALUES (?,?,?, ?, ?, ?, ?)");
-        $stmt -> bind_param("sssssss", $new_user, $new_pass,$new_func , $new_cpf, $new_ende, $new_cont, $new_email);
+    $novo_email = $_POST['novo_email'] ?? "";
+    $nova_senha = $_POST['nova_senha'] ?? "";
+    if($novo_email && $nova_senha){
+        $stmt = $conn -> prepare("INSERT INTO usuario (nome, funcao, cpf, endereco, contato, email, senha) VALUES (?,?,?, ?, ?, ?, ?)");
+        $stmt -> bind_param("sssssss", $new_user, $new_func , $new_cpf, $new_ende, $new_cont, $novo_email, $nova_senha,);
         
         if($stmt->execute()) {
             $register_msg = "Usuário cadastrado com sucesso!";
@@ -54,47 +54,50 @@ if($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['register'])){
         </div>
     </header>
     <main>
-        <div class="entradas">
-            <div class="nomeEntradas">NOME</div>
-            <div class="error" id="errorNome"></div>
-            <input class="boxEntradas" id="inputNome" name="nome" type="text"></input>
-        </div>
-        <div class="entradas">
-            <select name="funcao">
-                <option value="administrador">ADM</option>
-                <option value="maquinista" selected>MAQUINISTA</option>
-            </select>
-            <div class="error" id="errorFuncao"></div>
-            <input class="boxEntradas" id="inputFuncao"></input>
-        </div>
-        <div class="entradas">
-            <div class="nomeEntradas">CPF</div>
-            <div class="error" id="errorCpf"></div>
-            <input class="boxEntradas" id="inputCpf" name="cpf"></input>
-        </div>
-        <div class="entradas">
-            <div class="nomeEntradas">ENDEREÇO</div>
-            <div class="error" id="errorEndereco"></div>
-            <input class="boxEntradas" id="inputEndereco" name="endereco"></input>
-        </div>
-        <div class="entradas">
-            <div class="nomeEntradas">CONTATO</div>
-            <div class="error" id="errorCtt"></div>
-            <input class="boxEntradas" id="inputCtt" name="contato"></input>
-        </div>
-        <div class="entradas">
-            <div class="nomeEntradas">EMAIL</div>
-            <div class="error" id="errorEmail"></div>
-            <input class="boxEntradas" id="inputEmail" name="email"></input>
-        </div>
-        <div class="entradas">
-            <div class="nomeEntradas">SENHA</div>
-            <div class="error" id="errorSenha"></div>
-            <input class="boxEntradas" id="inputSenha" name="senha" type="password"></input>
-        </div>
-        <div id="buttonCadastro">
-            <button type="submit" name="register" id="buttonTxt" value="1">CADASTRAR</button>
-        </div>
+        <form method="post">
+
+            <div class="entradas">
+                <div class="nomeEntradas">NOME</div>
+                <div class="error" id="errorNome"></div>
+                <input class="boxEntradas" id="inputNome" name="nome" type="text"></input>
+            </div>
+            <div class="entradas">
+                <div class="nomeEntradas">FUNÇÃO</div>
+                <select name="funcao">
+                    <option value="administrador">ADM</option>
+                    <option value="maquinista" selected>MAQUINISTA</option>
+                </select>
+                <div class="error" id="errorFuncao"></div>
+            </div>
+            <div class="entradas">
+                <div class="nomeEntradas">CPF</div>
+                <div class="error" id="errorCpf"></div>
+                <input class="boxEntradas" id="inputCpf" name="cpf"></input>
+            </div>
+            <div class="entradas">
+                <div class="nomeEntradas">ENDEREÇO</div>
+                <div class="error" id="errorEndereco"></div>
+                <input class="boxEntradas" id="inputEndereco" name="endereco"></input>
+            </div>
+            <div class="entradas">
+                <div class="nomeEntradas">CONTATO</div>
+                <div class="error" id="errorCtt"></div>
+                <input class="boxEntradas" id="inputCtt" name="contato"></input>
+            </div>
+            <div class="entradas">
+                <div class="nomeEntradas">EMAIL</div>
+                <div class="error" id="errorEmail"></div>
+                <input class="boxEntradas" id="inputEmail" name="novo_email"></input>
+            </div>
+            <div class="entradas">
+                <div class="nomeEntradas">SENHA</div>
+                <div class="error" id="errorSenha"></div>
+                <input class="boxEntradas" id="inputSenha" name="nova_senha" type="password"></input>
+            </div>
+            <div id="buttonCadastro">
+                <button type="submit" name="register" id="buttonTxt" value="1">CADASTRAR</button>
+            </div>
+        </form>
     </main>
 </body>
 </html>
