e53b6001e82a355c8394cae6f18e9752df9297fb
Mateusbrum01 <mateus_cagneti@estudante.sesisenai.org.br>
2025-11-25 08:48:50 -0300

    Criei o ViaCEP/API

---
+++ scripts/viacep.js
@@ -0,0 +1,17 @@
+const cepInput = document.getElementById('cep');
+if (cepInput) {
+  cepInput.addEventListener('blur', viacepBuscarCEP);
+}
+
+function limparCampos() {
+  const ids = ['logradouro', 'bairro', 'localidade', 'uf', 'complemento'];
+  ids.forEach(id => {
+    const el = document.getElementById(id);
+    if (el) el.value = '';
+  });
+}
+
+function viacepBuscarCEP() {
+  const cep = (document.getElementById('cep')?.value || '').replace(/\D/g, '');
+  let form = document.getElementById('viacep-form');
+  if (!form && cepInput) form = cepInput.closest('form');
